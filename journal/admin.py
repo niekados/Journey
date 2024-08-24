8ab0e29efeca5a2d4392344cae3580fab7406a79
Vilmantas Zaleckas <tiny.post@yahoo.com>
2024-08-24 19:55:52 +0000

    Add CSRF_TRUSTED_ORIGINS to settings.py

--- journal/admin.py
+++ journal/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# Due to privacy JournalEntry model is not registered.
+# Admin should not have access to private journal entries.
