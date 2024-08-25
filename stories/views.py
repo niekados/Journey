d1fb959da83a2e876fe2c88fe8d33f5a7aaaae63
Vilmantas Zaleckas <tiny.post@yahoo.com>
2024-08-25 14:37:15 +0000

    Create stories views

--- stories/views.py
+++ stories/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import StoryEntry
 
-# Create your views here.
+
+class Stories(ListView):
+    """
+    Displays a list of published stories, ordered by the most recent
+    publication date, with pagination.
+    """
+    model = StoryEntry
+    queryset = StoryEntry.objects.filter(status=1).order_by('-published_on')
+    template_name = "stories/stories.html"
+    context_object_name = "stories_list"
+    paginate_by = 3
