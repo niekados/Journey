b61e6e2903d1201532eaa79aee0f08969bad0c71
Vilmantas Zaleckas <tiny.post@yahoo.com>
2024-08-25 17:05:19 +0000

    Create journal_detail template

--- journal/urls.py
+++ journal/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Journal, AddJournalEntry
+from .views import Journal, AddJournalEntry, JournalDetail
 
 urlpatterns = [
     path("", Journal.as_view(), name="journal"),
     path("add/", AddJournalEntry.as_view(), name="add_journal_entry"),
+    path("<int:pk>/", JournalDetail.as_view(), name="journal_detail"),
 ]
