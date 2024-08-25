346e7896b6d313da72129afb5d0dc7561b4dcd3c
Vilmantas Zaleckas <tiny.post@yahoo.com>
2024-08-25 11:10:44 +0000

    Setup django-allauth

--- journey/urls.py
+++ journey/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('journal/', include('journal.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('journal/', include('journal.urls')),
 ]
