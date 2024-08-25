acf9accaf3575259b7ab99fc6cba3243fbd5b724
Vilmantas Zaleckas <tiny.post@yahoo.com>
2024-08-25 17:26:22 +0000

    Create journal_entry_confirm_delete template

--- journal/urls.py
+++ journal/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
-from .views import Journal, AddJournalEntry, JournalDetail
+from .views import (
+    Journal,
+    AddJournalEntry,
+    JournalDetail,
+    delete_journal_entry
+)
 
 urlpatterns = [
     path("", Journal.as_view(), name="journal"),
     path("add/", AddJournalEntry.as_view(), name="add_journal_entry"),
     path("<int:pk>/", JournalDetail.as_view(), name="journal_detail"),
+    path(
+        "delete/<int:pk>/",
+        delete_journal_entry,
+        name="delete_journal_entry"
+    ),
 ]
